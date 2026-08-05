3a88df4977b6c5137422dd30c50c60b8c7ae76c3
Jasowant jat <jatajasabanta@gmail.com>
2026-08-05 09:48:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById("gameCanvas");
+const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const scoreEl = document.getElementById("score");
